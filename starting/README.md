3d7c860dd86409e9383aa3124a083bbd46f55fd1
LeoViolet <lleonardoo.silvaa90@gmail.com>
2025-05-29 10:17:47 +0100

    Inicio da aula03

--- starting/README.md
+++ starting/README.md
@@ -16,3 +16,8 @@ OBS: Inicio ao conhecimento sobre API de rotas
 1º No arquivo app.routes.ts é o local das nossas configurações globais
 falaremos sobre routelink-"...." que fica dentro do componente header.html
 temos que importar seu "Module"
+# aula03
+configurar o app main e footer
+instalação do LUXON
+Criação do construtor do Angular
+Inicia á injeção de dependência
--- starting/src/app/components/body/footer/footer.component.ts
+++ starting/src/app/components/body/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  private dataTime;
+  private routerinject = inject(Router); // Cria a nova forma de injeção
+  // cria a forma tradicional de injeção usado nos services ou components
+  constructor(private router: Router) {
+    //35
+    this.dataTime = DateTime.now()
+  };
+  // Construtor do Angular
+  ngOnInit(): void {
 
+  };
+  /**Uso quando tenho um botão no html que precisa mudar de rota assim que for clicado */
+  ChangeRoute () {
+    this.router.navigateByUrl('/hit-class/project')
+    this.routerinject.navigateByUrl('/hit-class/project')
+  };
+  /**Uso quando tenho um botão no html que precisa mudar de rota assim que for clicado */
+  ChangeRouterN = () =>  {
+    this.router.navigate(['/hit-class/project'])
+    this.routerinject.navigate(['/hit-class/project'])
+  };
 }
