49662b2aa88d108af54c7450040e484ad7119659
LeoViolet <lleonardoo.silvaa90@gmail.com>
2025-06-23 10:26:43 +0100

    Css do reqres.component atualizado, http-error-500.component realizado

--- starting/src/app/pages/api/api.routes.ts
+++ starting/src/app/pages/api/api.routes.ts
@@ -5,4 +5,5 @@ export const apiRoutes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'weather', loadComponent:() => import('./weather/weather.component').then(cChild => cChild.WeatherComponent)},
     {path: 'reqres', loadComponent:() => import('./reqres/reqres.component').then(cChild => cChild.ReqresComponent)},
+    {path: 'http-error-500', loadComponent:() => import('./http-error-500/http-error-500.component').then(cChild => cChild.HttpError500Component)},
 ]
---
+++ starting/src/app/pages/api/http-error-500/http-error-500.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-http-error-500',
+  imports: [],
+  templateUrl: './http-error-500.component.html',
+  styleUrl: './http-error-500.component.css'
+})
+export class HttpError500Component {
+
+}
